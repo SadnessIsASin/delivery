ddc5468f1fc66fd5edc06532326b7a645a1c71c6
Paul <sadnessisasin@gmail.com>
2026-05-09 22:43:04 +0300

    assign courier service added

--- src/main/java/com/hereyougo/delivery/assign/service/CourierAssignmentService.java
+++ src/main/java/com/hereyougo/delivery/assign/service/CourierAssignmentService.java
@@ -8,41 +8,80 @@ import com.hereyougo.delivery.order.entity.Order;
 import com.hereyougo.delivery.order.entity.OrderStatus;
 import com.hereyougo.delivery.order.repository.OrderRepository;
 import com.hereyougo.delivery.order.service.OrderService;
+import jakarta.transaction.Transactional;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.UUID;
 
 @Service
+@Slf4j
 public class CourierAssignmentService {
 
     private final OrderRepository orderRepository;
-    private final OrderService orderService;
+    private final CourierRepository courierRepository;
 
-     public CourierAssignmentService(
+    public CourierAssignmentService(
              OrderRepository orderRepository,
-             OrderService orderService) {
+             CourierRepository courierRepository) {
         this.orderRepository = orderRepository;
-        this.orderService = orderService;
+        this.courierRepository = courierRepository;
     }
 
     @Scheduled(fixedDelay = 5000)
     public void assignCourier () {
-        List <Order> newOrders = searchNewOrder();
-        if(newOrders.size() > 0) {
-            for(int i = 0; i < newOrders.size(); i++){
-                UUID orderId = newOrders.get(i).getId();
-                UpdateOrderDto dto = new UpdateOrderDto();
-                dto.setPickupAddress("автоматический адрес");
-                orderService.patchOrder(orderId, dto);
-            }
+        log.info("Поиск новых заказов");
+        List<Order> newOrders = searchNewOrders();
+        if(newOrders.isEmpty()){
+            log.info("Новых заказов нет");
+            return;
         }
+
+        log.info("Найдено новых заказов: {}", newOrders.size() );
+
+        log.info("Поиск свободных курьеров");
+        List<Courier> freeCouriers = searchFreeCouriers();
+        if(freeCouriers.isEmpty()){
+            log.info("Свободных курьеров нет");
+            return;
+        }
+
+        log.info("Найдено свободных курьеров {}", freeCouriers.size());
+
+        for (int i = 0; i < newOrders.size() && i < freeCouriers.size(); i++){
+            Order order = newOrders.get(i);
+            Courier courier = freeCouriers.get(i);
+
+            try {
+                assignCourierToOrder(order, courier);
+            } catch (Exception e){
+                e.printStackTrace();
+            }
+
+        }
+
     }
 
-    public List<Order> searchNewOrder () {
+    @Transactional
+    public void assignCourierToOrder (Order order, Courier courier) {
+        order.setCourier(courier.getId());
+        order.setUpdatedAt(LocalDateTime.now());
+        courier.setStatus(CourierStatus.BUSY);
+        courier.setUpdatedAt(LocalDateTime.now());
+        orderRepository.save(order);
+    }
+
+    public List<Order> searchNewOrders () {
         List<Order> newOrders = orderRepository.findByStatusAndCourierIsNull(OrderStatus.NEW);
         return newOrders;
     }
+
+    public List<Courier> searchFreeCouriers(){
+         List<Courier> freeCouriers = courierRepository.findCourierByStatus(CourierStatus.FREE);
+         return freeCouriers;
+    }
+
 }
